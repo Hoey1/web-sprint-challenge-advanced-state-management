74c0efe14bb153cc4651706e67349b00cbdc8c81
Joey <joey@xm8.ninja>
2020-07-17 13:20:34 -0400

    MVP done, not sure what my error was but it is working now

---
+++ smurfs/src/components/SmurfCard.js
@@ -0,0 +1,39 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { getSmurfs } from "../actions";
+
+const SmurfCard = (props) => {
+  useEffect(() => {
+    props.getSmurfs();
+  }, []);
+  console.log(props.smurfs);
+  const handleFetch = (e) => {
+    e.preventDefault();
+    props.getSmurfs();
+  };
+
+  return (
+    <div>
+      <div>
+        {props.smurfs.map((smurf) => {
+          return (
+            <div key={smurf.id}>
+              <h2> {smurf.name} </h2>
+              <p> {smurf.age} </p>
+              <p> {smurf.height} </p>
+            </div>
+          );
+        })}
+      </div>
+      <button onClick={handleFetch}> View the Village </button>
+    </div>
+  );
+};
+const mSTP = (state) => {
+  return {
+    smurfs: state.smurfs,
+    isFetchingData: state.isFetchingData,
+  };
+};
+
+export default connect(mSTP, { getSmurfs })(SmurfCard);
